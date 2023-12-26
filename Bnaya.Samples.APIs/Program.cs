ec8c8fbd4bf8287291150be99deae041dd82ccb5
Bnaya Eshet <bnayaes@payoneer.com>
2023-12-26 11:00:13 +0200

    basic instrumentation

--- Bnaya.Samples.APIs/Program.cs
+++ Bnaya.Samples.APIs/Program.cs
@@ -1,5 +1,9 @@
 using Bnaya.Samples.Common;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using StackExchange.Redis;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +21,44 @@ services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("loc
 // Register the event publisher
 services.AddSingleton<IEventPublisher, RedisEventPublisher>();
 
+#region Open Telemetry
+
+string serviceName = Assembly.GetExecutingAssembly()?.GetName().Name ?? throw new Exception("Assembly name");
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    var resource = ResourceBuilder.CreateDefault()
+                .AddService(serviceName);
+    options.SetResourceBuilder(resource
+                    .AddService(serviceName));
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource =>
+            resource.AddService(serviceName))
+
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddGrpcClientInstrumentation()
+          .AddRedisInstrumentation()
+          .AddSqlClientInstrumentation()
+          .AddOtlpExporter()
+          //.AddConsoleExporter()
+          .SetSampler<AlwaysOnSampler>())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddConsoleExporter()
+          .AddOtlpExporter()
+          .AddPrometheusExporter());
+//.AddConsoleExporter()
+
+#endregion // Open Telemetry
 
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Bnaya.Samples.Jobs/Program.cs
+++ Bnaya.Samples.Jobs/Program.cs
@@ -1,7 +1,11 @@
 using Bnaya.Samples.Common;
 using Bnaya.Samples.Jobs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using StackExchange.Redis;
 using System;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +22,45 @@ services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("loc
 services.AddSingleton<IEventHandler, MyEventHandler>();
 services.AddHostedService<Job>();
 
+#region Open Telemetry
+
+string serviceName = Assembly.GetExecutingAssembly()?.GetName().Name ?? throw new Exception("Assembly name");
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    var resource = ResourceBuilder.CreateDefault()
+                .AddService(serviceName);
+    options.SetResourceBuilder(resource
+                    .AddService(serviceName));
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource =>
+            resource.AddService(serviceName))
+
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddGrpcClientInstrumentation()
+          .AddRedisInstrumentation()
+          .AddSqlClientInstrumentation()
+          .AddOtlpExporter()
+          //.AddConsoleExporter()
+          .SetSampler<AlwaysOnSampler>())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddConsoleExporter()
+          .AddOtlpExporter()
+          .AddPrometheusExporter());
+          //.AddConsoleExporter()
+
+#endregion // Open Telemetry
+
+
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
